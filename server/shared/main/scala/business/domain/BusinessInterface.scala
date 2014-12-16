87930c443460ed5fb13642a12d55db64eb35841e
SemanticBeeng <nick@semanticbeeng.com>
2014-12-16 15:07:52 -0800

    wip: business logic

--- server/shared/main/scala/business/domain/BusinessInterface.scala
+++ server/shared/main/scala/business/domain/BusinessInterface.scala
@@ -2,13 +2,23 @@ package business.domain
 
 case class EmailAddress(name: String, domain: String)
 
-case class Customer(firstName: String, lastName: String, emails: List[EmailAddress])
+case class Customer(id: Long, firstName: String, lastName: String, emails: List[EmailAddress])
 
 case class BusinessException(errorCode: Int)
 
-trait BusinessMessages {
-  val CUSTOMER_ALREADY_EEXISTS = "Customer already exists"
-  val NO_CONTACT_INFO = "No contact info provided"
+/**
+ *
+ */
+object BusinessMessages {
+
+  final val CUSTOMER_ALREADY_EXISTS:Int = 1001
+  final val NO_CONTACT_INFO = 1002
+
+  def messageFor(errorCode: Int) = errorCode match {
+    case CUSTOMER_ALREADY_EXISTS => "Customer already exists"
+    case NO_CONTACT_INFO => "No contact info provided"
+    case _ => "Unknown error"
+  }
 }
 
 trait CustomerMgmtService {
@@ -16,3 +26,20 @@ trait CustomerMgmtService {
   def registerCustomer(customer: Customer): Either[Customer, List[BusinessException]]
 }
 
+trait CustomerValidator {
+
+  def validate(customer: Customer) : List[BusinessException] = {
+    var errors = List[BusinessException]() 
+
+    customer match {
+
+      case Customer(id, _*) if id != 0 => errors = errors :+ BusinessException(BusinessMessages.CUSTOMER_ALREADY_EXISTS)
+
+      case Customer(_, _, _, List()) => errors = errors :+ BusinessException(BusinessMessages.NO_CONTACT_INFO)
+
+      //case _ => Left(internalPersist(customer))
+    }
+    errors
+  }
+
+}
--- server/src/main/scala/business/server/Server.scala
+++ server/src/main/scala/business/server/Server.scala
@@ -1,11 +1,78 @@
+import business.domain._
+import business.server.CustomerValidator
 import upickle._
 import spray.routing.SimpleRoutingApp
 
 
+object CustomerMgmtServiceImpl extends CustomerMgmtService with CustomerValidator {
+
+  /**
+   *
+   * @param customer
+   * @return
+   */
+  override def registerCustomer(customer: Customer): Either[Customer, List[BusinessException]] = {
+
+    val errors: List[BusinessException] = validate(customer)
+
+    if (errors.isEmpty)
+      Left(internalPersist(customer))
+    else
+      Right(errors)
+  }
+
+  /**
+   *
+   * @param customer
+   */
+  def internalPersist(customer: Customer): Customer = {
+
+    print("Persisting " + customer.firstName)
+    Customer(10, customer.firstName, customer.lastName, customer.emails)
+  }
+}
 
 
+object AutowireServer extends autowire.Server[Customer, upickle.Reader[Customer], upickle.Writer[Customer]] {
+  def read[Result: upickle.Reader](p: Customer) = upickle.read[Result](p)
 
-object AutowireServer extends autowire.Server[String, upickle.Reader, upickle.Writer]{
-  def read[Result: upickle.Reader](p: String) = upickle.read[Result](p)
   def write[Result: upickle.Writer](r: Result) = upickle.write(r)
 }
+
+
+//object Server extends SimpleRoutingApp with CustomerMgmtService {
+//  def main(args: Array[String]): Unit = {
+//    //implicit val system = ActorSystem()
+//    startServer("0.0.0.0", port = 8080) {
+//      get{
+//        pathSingleSlash {
+//          complete{
+//            HttpEntity(
+//              MediaTypes.`text/html`,
+//              Template.txt
+//            )
+//          }
+//        } ~
+//          getFromResourceDirectory("")
+//      } ~
+//        post {
+//          path("api" / Segments){ s =>
+//            extract(_.request.entity.asString) { e =>
+//              complete {
+//                AutowireServer.route[Api](Server)(
+//                  autowire.Core.Request(s, upickle.read[Map[String, String]](e))
+//                )
+//              }
+//            }
+//          }
+//        }
+//    }
+//  }
+//
+//  def list(path: String): Seq[String] = {
+//    val chunks = path.split("/", -1)
+//    val prefix = "./" + chunks.dropRight(1).mkString("/")
+//    val files = Option(new java.io.File(prefix).list()).toSeq.flatten
+//    files.filter(_.startsWith(chunks.last))
+//  }
+//}
